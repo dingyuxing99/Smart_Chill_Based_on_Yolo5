47d8a4a3b74d345a1f02bf8490864d671fb40731
breezewrf <578450426@qq.com>
2023-03-24 22:32:40 +0800

    disable the login function

--- app/src/main/java/com/example/finalpoject/LoginActivity.java
+++ app/src/main/java/com/example/finalpoject/LoginActivity.java
@@ -28,19 +28,21 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = binding.loginEmail.getText().toString();
                 String password = binding.loginPassword.getText().toString();
-
-                if(email.equals("") || password.equals(""))
-                    Toast.makeText(LoginActivity.this,"All fields are mandatory",Toast.LENGTH_SHORT).show();
-                else{
-                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email,password);
-                    if(checkCredentials == true){
-                        Toast.makeText(LoginActivity.this,"Login Successfully",Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                    }else{
-                        Toast.makeText(LoginActivity.this,"Invalid Credentials",Toast.LENGTH_SHORT).show();
-                    }
-                }
+                Toast.makeText(LoginActivity.this,"Login Successfully",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+//                if(email.equals("") || password.equals(""))
+//                    Toast.makeText(LoginActivity.this,"All fields are mandatory",Toast.LENGTH_SHORT).show();
+//                else{
+//                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email,password);
+//                    if(checkCredentials == true){
+//                        Toast.makeText(LoginActivity.this,"Login Successfully",Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                        startActivity(intent);
+//                    }else{
+//                        Toast.makeText(LoginActivity.this,"Invalid Credentials",Toast.LENGTH_SHORT).show();
+//                    }
+//                }
             }
         });
         binding.signupRedirectText.setOnClickListener(new View.OnClickListener() {
